b561cc7023f4cc4b2f45be42c2e382c9f2690b6d
Tomé Rivero Lucas <ltome@grupost.com.ar>
2024-08-25 12:00:41 -0300

    primera subida del stitio, con solo el front, creacion de personajes a medias, tirada de dados a medias, chat a medias, mapa sencillo

---
+++ public/server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('Nuevo usuario conectado');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
